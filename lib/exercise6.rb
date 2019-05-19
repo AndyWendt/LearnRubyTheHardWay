99bf715c39d7c213e579270f9c72dc02cce08398
Andy Wendt <andy@awendt.com>
2019-05-19 15:34:01 -0600

    Add exercise 6

---
+++ lib/exercise6.rb
@@ -0,0 +1,26 @@
+# https://learnrubythehardway.org/book/ex6.html
+#
+
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}"
+
+puts x
+puts y
+
+# can't use expressions in single quoted strings
+# you have to use double quoted strings for expressions
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side"
+
+puts w + e
