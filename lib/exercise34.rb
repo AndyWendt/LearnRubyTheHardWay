988584bad512214a24a528f6e9243614d32593e9
Andy Wendt <andy@awendt.com>
2019-05-19 19:45:44 -0600

    Add exercise 34

---
+++ lib/exercise34.rb
@@ -0,0 +1,28 @@
+# https://learnrubythehardway.org/book/ex34.html
+#
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+puts animals[1]
+
+# The third (3rd) animal.
+puts animals[2]
+
+# The first (1st) animal.
+puts animals[0]
+
+# The animal at 3.
+puts animals[3]
+
+#  The fifth (5th) animal.
+puts animals[4]
+
+# The animal at 2.
+puts animals[2]
+
+# The sixth (6th) animal.
+puts animals[5]
+
+# The animal at 4
+puts animals[4]
